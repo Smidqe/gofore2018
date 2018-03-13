afbdf2f421d0b0051a1bfa9ccd95fe0d07d15062
Smidqe <smidqe@hotmail.fi>
2018-03-13 14:01:01 +0200

    Added couple comments to server.py

--- server.py
+++ server.py
@@ -3,27 +3,30 @@ import flask
 
 from flask import request
 from recipebook import RecipeBook
-from recipe import Recipe
 
+#setup the necessary variables
 app = flask.Flask(__name__)
 book = RecipeBook()
 book.load()
 
 '''
-    Very simplistic flask server
+    Very simplistic flask server, handles the GET requests coming to /search address
 
-
+    Required query params for /search:
+        - ingredient: string
 '''
 
 
 @app.route('/search', methods=['GET'])
 def search():
     result = None
+    #could have some checks about headers here?
 
     #check if the ingredient is in the query parameters
     if ('ingredient' in request.args):
         result = book.search('ingredient', request.args['ingredient'])
 
+    #jsonify the response and send it to the requester
     return flask.jsonify(result)
 
 
