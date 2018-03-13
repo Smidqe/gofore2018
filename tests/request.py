a9af1ed320918cb27cfb4761c3fe6a81d0228869
Smidqe <smidqe@hotmail.fi>
2018-03-13 14:01:36 +0200

    Modified a comment

--- tests/request.py
+++ tests/request.py
@@ -6,5 +6,5 @@ query = {'ingredient': 'pizza'}
 #send a GET request to locally run flask server
 r = requests.get('http://localhost:5000/search', params=query)
 
-#print the gotten json
+#print the gotten json to ensure we got what we wanted
 print(r.json())
